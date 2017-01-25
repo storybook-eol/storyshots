bd2351fc782b263a8bc0443d5ee0344da14dcf4d
John Rodriguez <john@John-Rodriguezs-MacBook-Pro.local>
2017-01-25 17:58:51 -0500

    Allow passing options to react-test-renderer .create() method

---
+++ stories/required_with_context/ComponentWithRef.stories.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { storiesOf, action } from '@kadira/storybook'
+import ComponentWithRef from './ComponentWithRef'
+
+storiesOf('Component with ref', module)
+  .add('on mount', () => (
+    <ComponentWithRef onLoad={action('component mount')} />
+  ))
